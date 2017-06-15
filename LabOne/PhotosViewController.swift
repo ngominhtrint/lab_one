ce14272770089a9642b935c1ce91e66f8720f632
Tri Ngo <ngominhtrint@gmail.com>
2017-06-15 13:37:07 +0700

    Milestone 2: Hook up the Tumblr API

--- LabOne/PhotosViewController.swift
+++ LabOne/PhotosViewController.swift
@@ -10,15 +10,43 @@ import UIKit
 
 class PhotosViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var photos: [NSDictionary]?
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        fetchPhotos()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func fetchPhotos() {
+        let url = URL(string: "https://api.tumblr.com/v2/blog/xkcn.info/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        
+        if let url = url {
+            let request = URLRequest(
+                url: url,
+                cachePolicy: NSURLRequest.CachePolicy.reloadIgnoringLocalCacheData,
+                timeoutInterval: 10)
+            let session = URLSession(
+                configuration: URLSessionConfiguration.default,
+                delegate: nil,
+                delegateQueue: OperationQueue.main)
+            let task = session.dataTask(
+                with: request,
+                completionHandler: { (dataOrNil, response, error) in
+                    if let data = dataOrNil {
+                        if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                            print("response: \(responseDictionary)")
+                            if let photoData = responseDictionary["data"] as? [NSDictionary] {
+                                self.photos = photoData
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+            })
+            task.resume()
+        }
     }
     
 
@@ -33,3 +61,26 @@ class PhotosViewController: UIViewController {
     */
 
 }
+
+extension PhotosViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension PhotosViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
+}
+
